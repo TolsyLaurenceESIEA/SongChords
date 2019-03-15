064177ac23a31d08d75758b19e9fa0c4baf2d8ee
TOLSY Laurence <tolsy@et.esiea.fr>
2019-03-15 12:17:46 +0100

    Initial Commit

--- app/src/main/java/com/example/songchords/View/TabActivity.java
+++ app/src/main/java/com/example/songchords/View/TabActivity.java
@@ -1,7 +1,6 @@
 package com.example.songchords.View;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
